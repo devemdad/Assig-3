798e42da160644257d0c7d01443520e4378df220
MD EMDADUL HAQUE <156192762+devemdad@users.noreply.github.com>
2025-01-17 19:02:49 +0600

    add get and post request

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Tipu")
+const express = require('express');
+
+const app = express(); 
+
+app.get("/", function(req, res){
+    res.send("Hello World");
+ });
+
+ app.post("/", function(req,res){
+    res.send("I am post body");
+})
+
+
+app.listen(5080, function(){
+    console.log("Server Run Success.") ;
+})
